1ad02a6ca5ac02c0acbbd5a2dc706b4230516eff
kamonoyoshiyasu <getugaku980yen@gmail.com>
2020-02-12 07:29:58 +0000

    kadai3

---
+++ kadai2.php
@@ -0,0 +1,24 @@
+<?php 
+$a =3;
+$b =7;
+echo $a+$b."\n";
+
+$array = ["January","Febrary","March","April","May","June","July","August","September","October","Novenber","December"];
+
+echo $array[7]."\n";
+
+$hello ="Hello";
+$name ="kamono";
+$world ="sworld";
+
+$tech_boost = 'tech';
+$tech_boost .= 'boost';
+
+echo $tech_boost."\n";
+
+
+
+$calendar_2018 = ["January" => "1月","February" => "2月","March" => "3月","April" => "4月","May" => "5月","June" => "6月","July" => "7月","August" => "8月","September" => "9月","October" => "10月","November" => "11月","December" => "12月"];
+
+// 12月を表示する
+echo $calendar_2018["December"];
